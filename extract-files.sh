0f60abc30c9c81cee8db8f937199108f1beaf4ff
chandu078 <chandudyavanapelli03@gmail.com>
2024-01-19 20:08:03 +0100

    fuxi: Change GPS NFW client to Google Carrier Location
    * Xiaomeme changed it to com.lbe.security.miui. This is a part of HyperOS fw, which we don't use in AOSP.
    
    Change-Id: Id8ceefab778fec72f143e3f3d7004a82fd98f428

--- extract-files.sh
+++ extract-files.sh
@@ -73,6 +73,9 @@ function blob_fixup() {
         vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service | vendor/lib64/hw/audio.primary.kalama.so)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
             ;;
+        vendor/etc/gps.conf)
+            sed -i 's/com\.lbe\.security\.miui/com\.google\.android\.carrierlocation/g' "${2}"
+            ;;
     esac
 }
 
