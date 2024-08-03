af6b5e83a0ce57cbba344c89b8f01bca53830d38
danielml <daniel@danielml.dev>
2024-08-03 18:11:00 +0200

    fuxi: Allow qwesd the pipe2 syscall
    Change-Id: I77d0947b5befaa2139c17e169a85cfe72d1ad311
    Signed-off-by: danielml <daniel@danielml.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,9 @@ function blob_fixup() {
         vendor/etc/gps.conf)
             sed -i 's/com\.lbe\.security\.miui/com\.google\.android\.carrierlocation/g' "${2}"
             ;;
+        vendor/etc/seccomp_policy/qwesd@2.0.policy)
+            echo "pipe2: 1" >> "${2}"
+            ;;
     esac
 }
 
