d78bf58c61f00f4cd0746c96b3918eb916c998a9
Akash Srivastava <akashniki@gmail.com>
2025-01-01 23:21:23 +0100

    fuxi: Fixup qwesd@2.0.policy
    * for some reason pipe2 wasn't being added causing the following crash:
    01-01 12:16:10.628     1     1 I init    : starting service 'vendor.qwesd'...
    01-01 12:16:10.646     1     1 I init    : ... started service 'vendor.qwesd' has pid 12531
    01-01 12:16:10.693     1     1 I init    : Service 'vendor.qwesd' (pid 12531) received signal 31
    01-01 12:16:10.693     1     1 I init    : Sending signal 9 to service 'vendor.qwesd' (pid 12531) process group...
    01-01 12:16:10.694     1     1 I libprocessgroup: Removed cgroup /sys/fs/cgroup/uid_1000/pid_12531
    01-01 12:16:10.694     1     1 E init    : process with updatable components 'vendor.qwesd' exited 4 times in 4 minutes

--- extract-files.sh
+++ extract-files.sh
@@ -104,7 +104,7 @@ function blob_fixup() {
         vendor/etc/qcril_database/upgrade/config/6.0_config.sql)
             sed -i '/persist.vendor.radio.redir_party_num/ s/true/false/g' "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/modemManager.policy | vendor/etc/seccomp_policy/qwesd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/modemManager.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
             [ "$2" = "" ] && return 0
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
@@ -115,7 +115,14 @@ function blob_fixup() {
             grep -q "setsockopt: 1" "${2}" || echo "setsockopt: 1" >> "${2}"
             ;;
         vendor/etc/seccomp_policy/qwesd@2.0.policy)
-            echo "pipe2: 1" >> "${2}"
+            # Remove trailing newline
+            sed -i '$ d' "$2"
+
+            # Append "gettid: 1" and "pipe2: 1"
+            {
+                echo "gettid: 1"
+                echo "pipe2: 1"
+            } >> "$2"
             ;;
         vendor/lib64/c2.dolby.client.so)
             "${PATCHELF}" --add-needed "libcodec2_hidl_shim.so" "${2}"
