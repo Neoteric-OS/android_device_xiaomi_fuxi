6a28936da191ba74735ee9e7a2f38c96e8090815
Akash Srivastava <akashniki@gmail.com>
2025-01-21 16:55:24 +0100

    fuxi: Build some more missing display libs

--- device.mk
+++ device.mk
@@ -162,7 +162,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.config \
+    vendor.qti.hardware.display.config-V2-ndk.vendor \
     vendor.qti.hardware.display.config-V5-ndk.vendor \
+    vendor.qti.hardware.display.config-V6-ndk.vendor \
     vendor.qti.hardware.display.demura-service \
     vendor.qti.hardware.display.mapper@1.0.vendor \
     vendor.qti.hardware.display.mapper@1.1.vendor \
